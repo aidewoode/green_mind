6c33688dc90643c3f48392f691460f0f43c2164a
aidewoode <aidewoode@gmail.com>
2015-09-17 23:44:22 +0800

    add workflow

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var csslint = require('gulp-csslint');
+var scsslint = require('gulp-scss-lint');
+
+gulp.task('watch_change', function() {
+  gulp.watch('scss/**/*.scss', function(event) {
+    gulp.src('scss/green_mind.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('docs/source/stylesheets'));
+  });
+});
+
+gulp.task('minify_css', function() {
+  gulp.src('dist/css/green_mind.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('publish_css', function() {
+  gulp.src('scss/green_mind.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('csslint', function() {
+  gulp.src('dist/css/green_mind.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter());
+});
+
+gulp.task('scsslint', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(scsslint());
+});
+
